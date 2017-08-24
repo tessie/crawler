803ba3df78961d054e3ee31107b2945866789959
tessy <tessyjosep1992@gmail.com>
2017-08-24 23:38:35 +0530

    Basic skeleton of crawler

---
+++ crawler.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+from getpage import get_page
+
+
+class Crawler:
+
+    def __init__(self, depth=4):
+        self.depth = depth
+
+    def crawl(seed):
+        links = []
+        return links
