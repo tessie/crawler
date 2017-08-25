fc3e00935feb63ed928a3786954965c9d9f044be
tessy <tessyjosep1992@gmail.com>
2017-08-25 11:52:09 +0530

    Metho crawl urls

--- crawler.py
+++ crawler.py
@@ -1,5 +1,5 @@
-from bs4 import BeautifulSoup
 from getpage import get_page
+from bs4 import BeautifulSoup
 
 
 class Crawler:
@@ -7,11 +7,18 @@ class Crawler:
     def __init__(self, depth=4):
         self.depth = depth
 
-    def crawl(seed):
-        links = []
-        return links
+    def crawl(self, seed):
+        links = set(seed)
+        crawled = []
+        while len(crawled) < self.depth and links:
+            url = links.pop()
+            content = get_page(url)
+            scrapped_links = self.__get_links(content)
+            links.update(scrapped_links)
+            crawled.append(url)
+        return crawled
 
-    def __get_links(content):
+    def __get_links(self, content):
         links = []
         soup = BeautifulSoup(content, 'html.parser')
         for link in soup.find_all('a'):
